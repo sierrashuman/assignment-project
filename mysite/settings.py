6ebbbeb146b05955e479acfc579d1f116d7b5448
Declan Brady <drbrady8800@gmail.com>
2021-10-13 15:04:19 -0400

    Added initial dummy test

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e03w#nqsf#tlcc86k$w9ob#^spc6d_4ia_vf215lg77-2vbqhk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'a-15-assignment-organizer.herokuapp.com']
 
 
 # Application definition
