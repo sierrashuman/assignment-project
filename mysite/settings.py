6de7c76f4ad4260998c76fa8c79139c544d6a295
Declan Brady <drbrady8800@gmail.com>
2021-10-17 12:51:07 -0400

    Update function on dict

--- mysite/settings.py
+++ mysite/settings.py
@@ -161,10 +161,9 @@ try:
         import dj_database_url
         import psycopg2
         DATABASE_URL = os.environ['DATABASE_URL']
-
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-        print("TEST:", DATABASES)
+        DATABASES['default'].update(dj_database_url.config(conn_max_age=600, ssl_require=True))
+
         # Heroku
         import django_heroku
         django_heroku.settings(locals())
