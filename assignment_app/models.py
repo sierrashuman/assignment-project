6e3b6f1475c0e634d8b08517e90c249070d707d6
Vivian Ma <vivianywma@gmail.com>
2021-10-27 12:03:49 -0400

    add models and views of course

--- assignment_app/models.py
+++ assignment_app/models.py
@@ -5,19 +5,34 @@ from django_mysql.models import ListTextField
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    courses = modelbbs.ListTextField(
-        base_field=IntegerField(),
-        size=50
+    courses = models.ListCharField(
+        base_field=CharField(),
+        size=10,
+        max_length=(10*50)
     )
+
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
-    name = models.CharField(max_length=50)
+    course = models.CharField(max_length=50)
     course_id = models.IntegerField()
     professor = models.CharField(max_length=50)
     course_descrip = models.TextField()
     lecture_times = models.TextField()
-    prereqs = models.ListTextField(
-        base_field-IntegerField),
-        size-5,
-        max_length=(5*10)
+    prereqs = models.ListCharField(
+        base_field=CharField,
+        size=4,
+        max_length=(4*50)
     )
-    enrolled = models.ManyToManyField(User)
+    enrolled = models.ManyToManyField(User, through = 'Enrollment')
+
+    def __str__(self):
+        return self.course
+
+class Enrollment(models.Model):
+    course = models.ForeignKey(Course)
+    student = models.ForeignKey(User)
+
+    
+
--- assignment_app/views.py
+++ assignment_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import Http404
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.views.generic.edit import CreateView
+from django.utils import timezone
+from django.shortcuts import get_object_or_404, render
+
+from .models import Course
 
 # Create your views here.
+
+class ViewCourse(CreateView):
+    model = Course
+    template_name = 'templates/courses.html'
+    fields = ['name', 'courseID', 'professor', 
+    'course_descrip', 'lecture_times', 'prereqs', 'enrolled']
+
+    
