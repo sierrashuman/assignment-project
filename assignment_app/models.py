5b5f63a170ebc09a91f721c271530b1f916e920a
Sierra Shuman <sierrashuman24@gmail.com>
2021-11-01 20:37:12 -0400

    Created models

--- assignment_app/models.py
+++ assignment_app/models.py
@@ -1,44 +1,28 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib import admin
-#from django_mysql.models import ListTextField
+from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
-    course_id = models.IntegerField()
+    course_mnemonic = models.CharField(max_length=10) # For example, 'CS 3240'
     professor = models.CharField(max_length=50)
     course_description = models.TextField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-
+    monday = models.BooleanField()  # To keep track of what days it meets if we make a schedule
+    tuesday = models.BooleanField()
+    wednesday = models.BooleanField()
+    thursday = models.BooleanField()
+    friday = models.BooleanField()
 
     def __str__(self):
         return self.name
 
     
-# class User(models.Model):
-#     name = models.CharField(max_length=50)
-#     courses = models.ListCharField(
-#         base_field=CharField(),
-#         size=10,
-#         max_length=(10*50)
-#     )
-
-#     def __str__(self):
-#         return self.name
-
-#class Course(models.Model):
-    
- #   enrolled = models.ManyToManyField(User, through = 'Enrollment')
-
- #   def __str__(self):
-  #      return self.course
-
-# class Enrollment(models.Model):
-#     course = models.ForeignKey(Courses)
-#     student = models.ForeignKey(User)
-
+class Enrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     
 
